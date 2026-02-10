1fd103d7ab0b15e9fa59225fb0da022e8647d7f4
Mehul Kar <github@mehulkar.com>
2026-02-09 20:00:02 -0600

    update install script

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ function start_fresh() {
   mkdir -p $BACKUP_DIR
 }
 
-
 function dosymlink() {
   local source="$1"
   local target="$2"
@@ -21,8 +20,6 @@ function dosymlink() {
   echo "Symlinking $source to $HOME"
   ln -s $source $target
 }
-
-
 
 function copy_essentials() {
   for file in essentials/*; do
@@ -37,50 +34,63 @@ function copy_files() {
   dosymlink "$PWD/.claude/settings.json" "$HOME/$(basename helpful/claude.json)"
 }
 
-start_fresh
-copy_files
+function install_vim_plugins() {
+  if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    echo "Installing VIM plugins with vundle"
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+  else
+    echo "Skipping VIM plugins"
+  fi
+}
 
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-  echo "Installing VIM plugins with vundle"
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
-else
-  echo "Skipping VIM plugins"
-fi
+function default_shell() {
+  if [ $SHELL != '/bin/zsh' ]; then
+    echo "Make zsh the default shell"
+    chsh -s $(which zsh)
+  else
+    echo "zsh is already the default shell"
+  fi
+}
 
-if [ $SHELL != '/bin/zsh' ]; then
-  echo "Make zsh the default shell"
-  chsh -s $(which zsh)
-else
-  echo "zsh is already the default shell"
-fi
-
-echo "Install oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true
-
-echo "Add startup scripts to zsh"
-echo "if [ -f $HOME/.startup ]; then; source $HOME/.startup; fi" >> ~/.zshrc
-echo "if [ -f $HOME/.secret ]; then; source $HOME/.secret; fi" >> ~/.zshrc
+function zhrc_stuff() {
+  echo "Install oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true
+  echo "Add startup scripts to zsh"
+  echo "if [ -f $HOME/.startup ]; then; source $HOME/.startup; fi" >> ~/.zshrc
+  echo "if [ -f $HOME/.secret ]; then; source $HOME/.secret; fi" >> ~/.zshrc
+}
 
 # Copy sample gitconfig
-GITSOURCE="$PWD/gitconfig.sample"
-GITTARGET="$HOME/.gitconfig"
+function gitconfig_stuff() {
+  GITSOURCE="$PWD/gitconfig.sample"
+  GITTARGET="$HOME/.gitconfig"
 
-if [ -f $GITTARGET ] || [ -L $GITTARGET]; then
-  mv $GITTARGET $BACKUPDIR
-fi
+  if [ -f $GITTARGET ] || [ -L $GITTARGET]; then
+    mv $GITTARGET $BACKUPDIR
+  fi
 
-cp $GITSOURCE $GITTARGET
+  cp $GITSOURCE $GITTARGET
+}
 
-echo "Installing monokai.terminal theme"
-rm -rf ~/tmp/monokai.terminal || true
-mkdir -p ~/tmp && git clone git@github.com:stephenway/monokai.terminal.git ~/tmp/monokai.terminal
-open ~/tmp/monokai.terminal/Monokai.terminal
-echo "Open Terminal settings and set Monokai.terminal as the default in Profiles"
+function terminal_theme() {
+  echo "Installing monokai.terminal theme"
+  rm -rf ~/tmp/monokai.terminal || true
+  mkdir -p ~/tmp && git clone git@github.com:stephenway/monokai.terminal.git ~/tmp/monokai.terminal
+  open ~/tmp/monokai.terminal/Monokai.terminal
+  echo "Open Terminal settings and set Monokai.terminal as the default in Profiles"
+}
+
+start_fresh
+copy_files
+install_vim_plugins
+default_shell
+zhrc_stuff
+gitconfig_stuff
+terminal_theme
 
 echo "Other items:"
 echo
 echo "- Install HomeBrew: https://brew.sh"
-echo "- Install Volta: https://docs.volta.sh/guide/getting-started"
 echo "- Install VSCode: https://code.visualstudio.com/download"
 echo "- Setup SSH Keys: https://help.github.com/articles/adding-a-new-ssh-key-to-the-ssh-agent"
